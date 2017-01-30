3dfbc3b0c7dd9a178773c3d8bcfb9fba058ad052
Alvaro Sanmartin <me@alvarosanmartin.com>
2017-01-30 00:24:19 +0000

    Added Yelp_api file and imported it in Web.py

---
+++ yelp_api.py
@@ -0,0 +1,35 @@
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
+import os
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+def get_food (location, term):
+    auth = Oauth1Authenticator(
+        consumer_key= os.environ['CONSUMER_KEY'],
+        consumer_secret= os.environ['CONSUMER_SECRET'],
+        token= os.environ['TOKEN'],
+        token_secret= os.environ['VBX470-LmvHprdkopBXUuh7yKpg']
+    )
+
+    client = Client(auth)
+
+    params = {
+        'term': 'food',
+        'lang': 'en'
+    }
+
+    response = client.search(location, **params)
+
+    businesses = []
+
+    for business in response.businesses:
+        #print (business.name)
+        businesses.append({"name": business.name, 
+            "rating": business.rating, "phone": business.phone})
+    return businesses
+
+
+businesses = get_food ('New York City', 'food')
+print (businesses)
